84103fad833d6bbe4605cdc8309f8f85c74dd6cd
Robert Tolentino <robert.tolentino@terranovatrading.com.pe>
2024-05-04 00:23:05 -0500

    Init project

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv  # Instalar con pip install python-dotenv
+
+load_dotenv()  # Carga todo el contenido de .env en variables de entorno
+
+class Config:
+    SERVER_NAME = "localhost:7001"
+    DEBUG = True
+
+    # DATABASE_PATH = "app/database/comprobantes.db"
+    DB_TOKEN = os.environ.get("DB_TOKEN", "")  # Para Encriptar la DB
+    ENCRYPT_DB = True
+
+    TEMPLATE_FOLDER = "views/templates/"
+    STATIC_FOLDER = "views/static/"
