ff5ce592c7fac47a586890606020462b48829caa
Robert Tolentino M. <robert.tolentino@terranovatrading.com.pe>
2024-05-07 17:50:25 -0500

    Add init config

--- config.py
+++ config.py
@@ -4,12 +4,13 @@ from dotenv import load_dotenv  # Instalar con pip install python-dotenv
 load_dotenv()  # Carga todo el contenido de .env en variables de entorno
 
 class Config:
-    SERVER_NAME = "localhost:7001"
+    # SERVER_NAME = "localhost:5000"
     DEBUG = True
 
     # DATABASE_PATH = "app/database/comprobantes.db"
-    DB_TOKEN = os.environ.get("DB_TOKEN", "")  # Para Encriptar la DB
-    ENCRYPT_DB = True
+    # DB_TOKEN = os.environ.get("DB_TOKEN", "")  # Para Encriptar la DB
+    # ENCRYPT_DB = True
 
     TEMPLATE_FOLDER = "views/templates/"
     STATIC_FOLDER = "views/static/"
+    SECRET_KEY = 'dev'
