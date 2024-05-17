41b2bfdd6817d56abc797125694e475a0a76e148
Robert Tolentino <robert.tolentino@terranovatrading.com.pe>
2024-05-16 23:18:46 -0500

    Add html5 qrcode

--- src/views/static/js/form.js
+++ src/views/static/js/form.js
@@ -2,6 +2,28 @@ const formQr = document.getElementById("formQr");
 const inputDataQr = document.getElementById("dataQr");
 var clickSubmit = false;
 
+/** https://scanapp.org/html5-qrcode-docs/docs/intro */
+function onScanSuccess(decodedText, decodedResult) {
+  // handle the scanned code as you like, for example:
+  console.log(`Code matched = ${decodedText}`, decodedResult);
+  alert(decodedText)
+  alert(decodedResult)
+  inputDataQr.value = decodedText
+}
+
+function onScanFailure(error) {
+  // handle scan failure, usually better to ignore and keep scanning.
+  // for example:
+  console.warn(`Code scan error = ${error}`);
+}
+
+let html5QrcodeScanner = new Html5QrcodeScanner(
+  "reader",
+  { fps: 10, qrbox: {width: 250, height: 250} },
+  /* verbose= */ false);
+html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+/** */
+
 formQr.addEventListener("submit", (event) => {
   event.preventDefault();
   if (clickSubmit) {
