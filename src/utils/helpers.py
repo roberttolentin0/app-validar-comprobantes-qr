12bac04fa1d8c3b4911eb31ff59d7783fd97b582
Robert Tolentino M. <robert.tolentino@terranovatrading.com.pe>
2024-06-08 17:27:53 -0500

    Add parse list with signs ¿

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -24,11 +24,49 @@ def convert_to_postgres_format(amount_str):
     amount_str = amount_str.replace(',', '.')
     return amount_str
 
+# Función para extraer y convertir los valores
+def parse_list_with_signs(lista_entrada):
+    '''
+        param: lista_entrada: lista de strings con los valores a transformar
+            ex ['-fdig-index.php_vr¿20609834774', 'td¿01', 'nd¿0002780', 'vs¿F002', "fec¿2024'06'06", 'mon¿2301.67']
+        return: lista de valores transformados
+            ex ['20609834774', '01', 'F002', '0002780', '0', '2301.67', "2024'06'06"]
+        caso de ejemplo:
+            ROCADMG
+    '''
+
+    resultado = [None] * 7  # Inicializa la lista con None para las 7 posiciones
+    for item in lista_entrada:
+        clave, valor = item.split('¿')
+        if clave == '-fdig-index.php_vr':
+            resultado[0] = valor  # Convierte a entero y coloca en la posición 0
+        elif clave == 'td':
+            resultado[1] = valor  # Convierte a entero y coloca en la posición 1
+        elif clave == 'nd':
+            resultado[3] = valor  # Coloca en la posición 3
+        elif clave == 'vs':
+            resultado[2] = valor  # Coloca en la posición 2
+        elif clave == 'fec':
+            resultado[6] = valor  # Coloca en la posición 6
+        elif clave == 'mon':
+            valor = valor.replace("'", '') # Si '14.82
+            resultado[5] = valor  # Coloca en la posición 5
+
+    resultado[4] = 'No se muestra IGV'
+
+    return resultado
+
+
 def parse_qr_code(input_str):
     '''input_str: 20100127165|01|F100|00451161|99.61|653.06|2024-05-22|6|20609699982|hgH0bEf7HK57HHPG1p6ZihmbQvI='''
     # Separar la entrada usando el delimitador '|' or ']'
-    parts = re.split(r'\||\]', input_str)
+    parts = re.split(r'\||\]|\/', input_str)
     print(parts)
+
+    # Transformar parts de tipo ['-fdig-index.php_vr¿20609834774', 'td¿01', 'nd¿0002780', 'vs¿F002', "fec¿2024'06'06", 'mon¿2301.67']
+    if '¿' in parts[0]:
+        parts = parsed_list_with_signs(parts)
+        print('parts sin ¿ : ', parts)
 
     # Transformar parts si el RUC de la empresa está en la posición 3
     # Ex [6, 20602289029, 6, 20609699982, 01, F010-00001387, 2023-03-08, 102.97, 675.00, Pb4eBPZCAYkGBCh6FLKivNpPKAE=]
